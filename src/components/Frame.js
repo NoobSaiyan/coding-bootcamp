a36d12c64b4f338f5b825e5a03d68c93078fa937
NoobSaiyan <ms19961998@gmail.com>
2020-10-07 19:18:08 +0530

    stated desktop view, added image

---
+++ src/components/Frame.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../css/frame.css'
+
+const Frame = () => {
+  return (
+    <div className='image'>
+      <img src='/images/image-tanya.jpg' alt='' />
+    </div>
+  )
+}
+
+export default Frame
